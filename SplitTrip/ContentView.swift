28777014adddd60939adcd11dbea565de3136d23
AudyDev <audyDev@gmail.com>
2024-11-02 18:44:05 +0100

    Initial Commit

---
+++ SplitTrip/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SplitTrip
+//
+//  Created by AudyDev on 2/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SplitTrip/SplitTripApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplitTripApp.swift
+//  SplitTrip
+//
+//  Created by AudyDev on 2/11/24.
+//
+
+import SwiftUI
+
+@main
+struct SplitTripApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
